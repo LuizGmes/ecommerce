a5b5b95f23708dae64f8e231ce2cbe434521e65c
Luiz Gomes <luiz.silva@hivecloud.com.br>
2024-06-21 23:34:07 -0300

    centralizando serviços de mensagens, serializando pedidos

--- src/main/java/br/com/luiz/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/luiz/ecommerce/NewOrderMain.java
@@ -1,43 +1,27 @@
 
 package br.com.luiz.ecommerce;
 
-import org.apache.kafka.clients.producer.Callback;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
-
-import java.util.Properties;
+import java.math.BigDecimal;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String [] args) throws ExecutionException, InterruptedException {
-        KafkaProducer producer = new KafkaProducer<String, String>(properties());
+        try (KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>()) {
+            try (KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>()) {
+                for (int i = 0; i < 10; i++) {
+                    String userId = UUID.randomUUID().toString();
+                    String orderId = UUID.randomUUID().toString();
+                    BigDecimal amount = new BigDecimal(Math.random() * 5000 + 1);
 
-        final Callback callback = (data, ex) -> {
-            if (ex != null) {
-                ex.printStackTrace();
-                return;
+                    Order order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    String email = "Thank you for your order, We are processing your order";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
-            System.out.println("sucesso enviando " + data.topic() + ":::partition" + data.partition() + "/ offset" + data.offset() + "/ timestamp" + data.timestamp());
-        };
-
-        String orderKey = "PED_1317 - teste 3";
-        String orderValue = "132123,366954,1234";
-        ProducerRecord orderRecord = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", orderKey, orderValue);
-        producer.send(orderRecord, callback).get();
-
-        String emailKey = "EMAIL_0005 -teste 3";
-        String emailValue = "Thank you for your order, We are processing your order";
-        ProducerRecord emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", emailKey, emailValue);
-        producer.send(emailRecord, callback).get();
-    }
-    private static Properties properties() {
-        Properties properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return properties;
+        }
     }
 }
